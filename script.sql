b09526e77b7907fed35b0c3037b52d14e1de191c
Maryem Khaoua <maryem.khaoua@gmail.com>
2023-11-20 00:08:00 +0100

    Insert data to tables

--- script.sql
+++ script.sql
@@ -1,4 +1,6 @@
-CREATE TABLE Organisateur (
+-- creation des tableaux
+
+CREATE TABLE Organisateurs (
     id_org INT PRIMARY KEY,
     nom VARCHAR(50),
     email VARCHAR(50),
@@ -6,53 +8,73 @@ CREATE TABLE Organisateur (
 );
 
 CREATE TABLE Organiser (
-    id_org INT,
-    id_event INT,
-    PRIMARY KEY (id_org, id_event),
-    FOREIGN KEY (id_org) REFERENCES Organisateur(id_org),
-    FOREIGN KEY (id_event) REFERENCES Evenement(id_event)
+    id_org INT  PRIMARY KEY,
+    id_event INT  PRIMARY KEY,
+    FOREIGN KEY (id_org) REFERENCES Organisateurs(id_org),
+    FOREIGN KEY (id_event) REFERENCES Evenements(id_event)
 );
 
-CREATE TABLE Participant (
+CREATE TABLE Participants (
     id_part INT PRIMARY KEY,
     nom VARCHAR(50),
     email VARCHAR(50),
     tel VARCHAR(15)
 );
 
-CREATE TABLE Evenement (
+CREATE TABLE Evenements (
     id_event INT PRIMARY KEY,
     titre VARCHAR(100),
     date DATE,
     description TEXT,
     id_lieu INT,
-    FOREIGN KEY (id_lieu) REFERENCES Lieu(id_lieu)
+    FOREIGN KEY (id_lieu) REFERENCES Lieux(id_lieu)
 );
 
-CREATE TABLE Lieu (
+CREATE TABLE Lieux (
     id_lieu INT PRIMARY KEY,
     nom VARCHAR(50),
     adresse VARCHAR(100)
 );
 
-CREATE TABLE Sponsor (
+CREATE TABLE Sponsors (
     id_sponsor INT PRIMARY KEY,
     nom VARCHAR(50)
 );
 
 CREATE TABLE Sponsorer (
-    id_event INT,
-    id_sponsor INT,
-    PRIMARY KEY (id_event, id_sponsor),
-    FOREIGN KEY (id_event) REFERENCES Evenement(id_event),
-    FOREIGN KEY (id_sponsor) REFERENCES Sponsor(id_sponsor)
+    id_event INT PRIMARY KEY,
+    id_sponsor INT PRIMARY KEY,
+    FOREIGN KEY (id_event) REFERENCES Evenements(id_event),
+    FOREIGN KEY (id_sponsor) REFERENCES Sponsors(id_sponsor)
 );
 
-CREATE TABLE Billet (
+CREATE TABLE Billets (
     id_billet INT PRIMARY KEY,
     id_event INT,
     id_part INT,
-    FOREIGN KEY (id_event) REFERENCES Evenement(id_event),
-    FOREIGN KEY (id_part) REFERENCES Participant(id_part)
-
+    FOREIGN KEY (id_event) REFERENCES Evenements(id_event),
+    FOREIGN KEY (id_part) REFERENCES Participants(id_part)
 );
+
+-- Insert data to tables
+
+INSERT INTO Organisateurs (id_org, nom, email, tel)
+VALUES (1, 'Maryem', 'mym@gmail.com', '0623456789');
+
+INSERT INTO Participants (id_part, nom, email, tel)
+VALUES (1, 'Malak', 'malak@gmail.com', '0612345678');
+
+INSERT INTO Lieux (id_lieu, nom, adresse)
+VALUES (1, 'Youcode', 'Youssofia');
+
+INSERT INTO Sponsors (id_sponsor, nom)
+VALUES (1, 'OCP');
+
+INSERT INTO Organiser (id_org, id_event)
+VALUES (1, 1);
+
+INSERT INTO Sponsorer (id_event, id_sponsor)
+VALUES (1, 1);
+
+INSERT INTO Billets (id_billet, id_event, id_part)
+VALUES (1, 1, 1);
